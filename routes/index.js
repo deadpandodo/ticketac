477c06348fe9d361afec2f46b2d3248649c6fff7
Dim <dimitri@vooter.co>
2022-01-27 15:08:16 +0100

    payment modal

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ var router = express.Router();
 var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
 var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
+/* 
+TODO
+- User Sessions
+- take care of dates in basket
+
+ */
 
 
 /* INDEX */
@@ -102,14 +108,6 @@ router.get('/add-trip-to-basket', async function(req, res, next) {
   }
 
   console.log(basket)
-
-
-
-
-
-
-
-
  
   res.render('basket', { basket: basket });
 });
