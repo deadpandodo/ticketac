0170c31218a93d29f8d57a0de1c9376b43962030
Dim <dimitri@vooter.co>
2022-01-27 11:40:57 +0100

    includes

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,14 @@ var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { });
 });
 
 
-
+/* GET home page. */
+router.get('/homepage', function(req, res, next) {
+  res.render('homepage', {  });
+});
 
 
 
