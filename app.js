e775f3bec00189de0f44c423e57de4f49ea72047
Dim <dimitri@vooter.co>
2022-01-27 09:43:54 +0100

    3rd commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('./models/dbconnect');
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
---
+++ models/journeys.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+var journeySchema = mongoose.Schema({
+  departure: String,
+  arrival: String,
+  date: Date,
+  departureTime: String,
+  price: Number,
+});
+
+module.exports = mongoose.model('journey', journeySchema);
